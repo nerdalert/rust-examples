29ad64c284c251dbef1cf38295e444d87e9a399d
Brent Salisbury <brent.salisbury@gmail.com>
2019-12-18 00:44:04 -0500

    Added a string replace module

---
+++ find-replace/src/main.rs
@@ -0,0 +1,19 @@
+/// replace creates a new String, and copies the data from
+/// this string slice into it. While doing so, it attempts
+/// to find matches of a pattern. If it finds any, it
+/// replaces them with the replacement string slice.
+fn main() {
+    // option #1
+    let option1 = str::replace("192.168.1.100", ".", "-");
+    println!("{}", option1);
+    // option #2
+    let option2 = "docs.aws.amazon.com".replace(".", "-");
+    println!("{}", option2);
+}
+
+/*
+Example output:
+--------------
+192-168-1-100
+docs-aws-amazon-com
+*/
